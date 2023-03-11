ffb2b2fb82a7a13f561fdc51b93394a3c18cadbd
mek365 <126380074+mek365@users.noreply.github.com>
2023-03-11 21:17:04 +0900

    commit 4

--- speaker.py
+++ speaker.py
@@ -15,6 +15,7 @@ import speech_recognition as sr
 from gtts import gTTS
 from playsound import playsound
 
+import feedparser
 import re
 
 # 음성인식(듣기, STT)
@@ -29,26 +30,29 @@ def listen(recognizer, audio):
     except sr.RequestError as e:
         print('요청 실패 : {0}'.format(e)) # api key 오류, 네트워크 단절 등
 
+# 날씨 정보
 def weather_info() :                                                                                                           
                                                                                                                                
     url_weather = r"https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=%EB%82%A0%EC%94%A8" # 네이버 날씨  
-    html = requests.get(url_weather) # 데이터 보내기                                                                                          
+    html = requests.get(url_weather) # 데이터 요청
+    # html.text 형태로 데이터를 받으면 지저분하게 데이터가 받아짐
+    # BeautifulSoup을 사용해 데이터를 정리해서 받음                                                                                          
     soup = BeautifulSoup(html.text, 'html.parser')  # 응답받은 HTML 내용을 BeautifulSoup 클래스의 객체 형태로 생성/반환                                                                          
                                                                                                                                
-    #지역                                                                                                                      
+    # 지역                                                                                                                      
     location = soup.find('div', {'class': 'title_area _area_panel'}).find('h2', 'title').text                                  
-    #날씨 정보                                                                                                                 
+    # 날씨 정보                                                                                                                 
     weather = soup.find('div', {'class': 'weather_info'})                                                                      
-    #현재 온도                                                                                                                 
+    # 현재 온도                                                                                                                 
     temp = weather.find('div', {'class': 'temperature_text'}).text.strip()[5:-1]                                               
-    #날씨 상태                                                                                                                 
+    # 날씨 상태                                                                                                                 
     status = weather.find('span', {'class':'weather before_slash'}).text                                                       
-    #체감                                                                                                                      
+    # 체감                                                                                                                      
     term = weather.find('dl',  {'class':'summary_list'}).find('dd', {'class':'desc'})                                          
     term1 = term.text.strip()[:-1]                                                                                             
-    #습도                                                                                                                      
+    # 습도                                                                                                                      
     term2 = term.find_next('dd', {'class':'desc'}).text.strip()                                                                
-    #미세먼지                                                                                                                  
+    # 미세먼지                                                                                                                  
     report = weather.find('ul',  {'today_chart_list'}).text.strip()                                                            
                                                                                                                                
     today = f'오늘 {location} 날씨입니다. 현재 온도는 {temp}도, 체감 온도는 {term1}도, 습도 {term2} 입니다.'
@@ -56,6 +60,7 @@ def weather_info() :
                                                                                                                                
     return today   
 
+# 환율 정보
 def exchange_rate_info() :                                                                                                           
                                                                                                                                
     url_exchange_rate = r"https://finance.naver.com/marketindex/" # 네이버 환율  
@@ -74,6 +79,18 @@ def exchange_rate_info() :
                                                                                                                                
     return today_exchange_rate   
 
+# RSS - google news feed
+# Rich Site Summary : 새 기사들의 제목만, 또는 새 기사들 전체를 뽑아서 하나의 파일로 만들어 둔 것
+def news_info() :                                               
+    url = r"https://news.google.com/rss?hl=ko&gl=KR&ceid=KR:ko" 
+    news_data = []                                            
+    news_rss = feedparser.parse(url) # 파싱                  
+    for title in news_rss.entries :  # 엔트리에 딕셔너리 형태로 데이터가 담겨져 있음                         
+        news_data.append(title.title) 
+
+    today_news = f'오늘 주요 뉴스 3개를 알려드릴게요. {news_data[0:3]}. 이상입니다.'                                                   
+    return today_news  
+
 # 대답(분석해서 스피커가 어떤 대답을 할지)
 def answer(input_text):
     answer_text = ''
@@ -84,16 +101,18 @@ def answer(input_text):
         answer_text = weather_info()
     elif '환율' in input_text:
         answer_text = exchange_rate_info()
+    elif '뉴스' in input_text:
+        answer_text = news_info() 
     elif '고마워' in input_text:
         answer_text = '별말씀을요.'  
     elif '종료' in input_text:
         answer_text = '다음에 또 만나요.'   
-        stop_listening(wait_for_stop=False)   # 어떤 시점이 되면 듣지 않게 하기
-        print(stop_listening)
+        stop_listening = 'X'   # 어떤 시점이 되면 듣지 않게 하기
     else:
         answer_text = '다시 한 번 말씀해주시겠어요?'
 
     speak(answer_text)
+    return stop_listening
 
 # 컴퓨터가 소리내서 읽기(TTS)
 def speak(text):
@@ -128,7 +147,6 @@ m = sr.Microphone() # 마이크를 통해 소리를 받을 객체 만들기
 
 # 어떤 목소리가 들리면 바로 그 처리할 수 있게 도와줌
 stop_listening = r.listen_in_background(m, listen)
-print(stop_listening)
 
 if stop_listening == False:
     if os.path.exists(file_path):
